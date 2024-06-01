00df6058131ef699f442348f109863ace1df46ea
dai-trinh-nmn <2251061735@e.tlu.edu.vn>
2024-06-01 22:51:00 +0700

    Added search, api to get weather data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { WeatherService } from './services/weather.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  providers: [WeatherService]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'WeatherApp';
+  weatherData: any;
+
+  constructor(private weatherService:WeatherService) { }
+
+  cityName: string="Hanoi";
+
+  ngOnInit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = "";
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = "";
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe(data => {
+      this.weatherData = data;
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'http://api.openweathermap.org/data/2.5/weather?q=',
+    apiKey: 'bce0a639b1d2fdc1eeb7a0d121c4f1a9',
+  };
+  
