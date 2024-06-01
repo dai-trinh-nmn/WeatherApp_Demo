6c04cc242b9fa9bc47082b8be9bd67e9fdca745a
dai-trinh-nmn <2251061735@e.tlu.edu.vn>
2024-06-01 22:48:24 +0700

    Added search, api to get weather data

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName: string) {
+    return this.http.get(`${environment.weatherApiBaseUrl}${cityName}&appid=${environment.apiKey}`);
+  }
+}
